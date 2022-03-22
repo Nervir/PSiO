2bc2dbe8ffa760f45aac12ada080d4fce7da4ac3
Adam Stawecki <x.stawecki@gmail.com>
2022-03-22 18:03:52 +0100

    Update L02Z06

--- L02/Z06.cpp
+++ L02/Z06.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
 #include <vector>
 
-int main()
-{
-    std::vector<int> list = { 10, 15, 23, 8, 5, 9, 40, 11, 12, 13, 1 };
-    int list_size = list.size();
+void bubble_sort(std::vector<int>& list) {
     bool change;
     do {
         change = false;
-        for (int i = 0; i < list_size - 1; i++) {
+        for (int i = 0; i < list.size() - 1; i++) {
             if (list[i] > list[i + 1]) {
                 int temp = list[i + 1];
                 list[i + 1] = list[i];
@@ -17,6 +14,13 @@ int main()
             }
         }
     } while (change == true);
+}
+
+
+int main()
+{
+    std::vector<int> list = { 10, 15, 23, 8, 5, 9, 40, 11, 12, 13, 1 };
+    bubble_sort(list);
     for (auto i : list) {
         std::cout << i << ", ";
     }
