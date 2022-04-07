95ed99b8644c0ac03e11e53172b4ab0b77304608
Adam Stawecki <x.stawecki@gmail.com>
2022-04-07 07:57:28 +0200

    Add L03Z02

---
+++ L03/Z02.cpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+#include <iostream>
+#include <list>
+#include <random>
+#include <ctime>
+
+int randomInt(int min, int max) {
+    static std::default_random_engine e{ static_cast<long unsigned int>(time(0)) };
+    std::uniform_int_distribution<int> d{ min, max };
+    return d(e);
+}
+void print(std::list<int> l) {
+    for (int number : l) {
+        std::cout << number << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    const int n = 10;
+    std::list<int> numbers;
+
+    for (int i = 0; i < n; i++) {
+        numbers.emplace_back(randomInt(15, 20));
+    }
+
+    //for (int i = 0; i < numbers.size(); i++) {
+    //    std::cout << numbers[i] << ", ";
+    //}
+
+    print(numbers);
+
+    int to_find = 15;
+    //std::erase(numbers, to_find); //https://en.cppreference.com/w/cpp/container/list/erase2
+
+    numbers.remove(to_find);
+    
+    print(numbers);
+}
